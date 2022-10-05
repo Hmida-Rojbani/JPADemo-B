75878e5d9b0b6bdea6ae3314489b9684e4dd4246
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2022-10-05 11:02:06 +0100

    Create First Entity

---
+++ src/main/java/de/tekup/jpademob/JpaDemoBApplication.java
@@ -0,0 +1,13 @@
+package de.tekup.jpademob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaDemoBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaDemoBApplication.class, args);
+    }
+
+}
---
+++ src/main/java/de/tekup/jpademob/entities/EmployeeEntity.java
@@ -0,0 +1,11 @@
+package de.tekup.jpademob.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class EmployeeEntity {
+    @Id
+    private int id;
+
+}
