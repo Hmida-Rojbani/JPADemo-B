e307fdb1eac323eb7deb7f4f993741f6bb367c3e
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2022-10-05 11:18:40 +0100

    add columns

--- src/main/java/de/tekup/jpademob/entities/LibraryEntity.java
+++ src/main/java/de/tekup/jpademob/entities/LibraryEntity.java
@@ -1,11 +1,18 @@
 package de.tekup.jpademob.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="library")
 public class LibraryEntity {
     @Id
     private int id;
+    @Column(length = 50)
+    private String name;
+    @Column(length = 40,nullable = false,unique = true)
+    private String matricule;
 
 }
