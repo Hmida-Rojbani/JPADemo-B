5dbba78d2eaf71655965e181ea74e21ea6480f34
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2022-10-12 11:48:45 +0100

    empty classes

---
+++ src/main/java/de/tekup/jpademob/ctrl/BookCtrl.java
@@ -0,0 +1,7 @@
+package de.tekup.jpademob.ctrl;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BookCtrl {
+}
---
+++ src/main/java/de/tekup/jpademob/services/BookService.java
@@ -0,0 +1,8 @@
+package de.tekup.jpademob.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+}
