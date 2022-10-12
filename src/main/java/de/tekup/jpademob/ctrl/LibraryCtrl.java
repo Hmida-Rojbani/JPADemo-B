be852b8f848ce7f27850cfde9b6f3828c52153e5
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2022-10-12 09:49:12 +0100

    qdd insert into DB

--- src/main/java/de/tekup/jpademob/ctrl/LibraryCtrl.java
+++ src/main/java/de/tekup/jpademob/ctrl/LibraryCtrl.java
@@ -3,9 +3,7 @@ package de.tekup.jpademob.ctrl;
 import de.tekup.jpademob.entities.LibraryEntity;
 import de.tekup.jpademob.services.LibraryService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,9 +15,17 @@ public class LibraryCtrl {
     public LibraryCtrl(LibraryService libraryService) {
         this.libraryService = libraryService;
     }
-    @RequestMapping("/libraries/all")
+    //@RequestMapping(value = "/libraries/all",method = RequestMethod.GET)
+    @GetMapping("/libraries/all")
     @ResponseBody
     public List<LibraryEntity> getAll(){
         return libraryService.selectAll();
+    }
+
+    //@RequestMapping(path="/libraries/save",method = RequestMethod.POST)
+    @PostMapping("/libraries/save")
+    @ResponseBody
+    public LibraryEntity addLibrary(@RequestBody LibraryEntity library){
+        return libraryService.insertIntoDB(library);
     }
 }
--- src/main/java/de/tekup/jpademob/entities/LibraryEntity.java
+++ src/main/java/de/tekup/jpademob/entities/LibraryEntity.java
@@ -1,14 +1,12 @@
 package de.tekup.jpademob.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="library")
 public class LibraryEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(length = 50)
     private String name;
